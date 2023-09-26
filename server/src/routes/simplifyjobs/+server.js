d74ae137146d88eb76eb9fdf2e8b3a160d82bac7
Jasanpreet Nagra <jasanpreetn9@gmail.com>
2023-09-26 11:45:32 -0500

    Update +server.js

--- server/src/routes/simplifyjobs/+server.js
+++ server/src/routes/simplifyjobs/+server.js
@@ -1,75 +1,82 @@
 export const GET = async ({ request, url }) => {
-    const parseMarkdownTable = (markdownText) => {
-        const lines = markdownText.trim().split('\n');
-        const data = [];
-        let isDataSection = false;
+	const parseMarkdownTable = async (markdownText) => {
+		const headerSplitter =
+			'<!-- Please leave a one line gap between this and the table TABLE_START (DO NOT CHANGE THIS LINE) -->';
+		const footerSplitter =
+			'<!-- Please leave a one line gap between this and the table TABLE_END (DO NOT CHANGE THIS LINE) -->';
 
+		const lines = markdownText.trim().split('\n');
+		const data = [];
 
-        for (const line of lines) {
-            if (line.includes('| ----------- |')) {
-                isDataSection = true;
-                continue;
-            }
+		const headerIndex = lines.indexOf(headerSplitter);
+		lines.splice(0, headerIndex + 4);
 
-            if (isDataSection && line.includes('|')) {
-                const values = line
-                    .split('|')
-                    .map((item) => item.trim())
-                    .filter((item) => item !== '');
+		const footerIndex = lines.indexOf(footerSplitter);
+		lines.splice(footerIndex, lines.length - 1);
 
-                if (values.length >= 5) {
-                    let locations = values[2].trim().split('</br>');
-                    locations = locations.map((location) =>
-                        location
-                            .replace(/<details><summary>.*?<\/summary>/g, '')
-                            .replace(/<\/details>/g, '')
-                            .trim()
-                    );
+		for (let i = lines.indexOf(headerSplitter) + 1; i < footerIndex; i++) {
+			let line = lines[i].trim();
+			if (line.startsWith('|')) {
+				line = line.substring(1); // Remove the leading '|' if present
+			}
+			if (line.endsWith('|')) {
+				line = line.slice(0, -1); // Remove the trailing '|' if present
+			}
+			const values = line.split('|').map((value) => value.trim());
 
-                    let link = values[3].trim().split('href="')[1];
+			// Check if values[0] contains a Markdown link
+			let company = values[0];
+			if (company.startsWith('**[') && company.endsWith(')**')) {
+				// Extract the company name and link URL
+				const linkMatches = company.match(/\*\*\[(.*?)\]\((.*?)\)\*\*/);
+				if (linkMatches && linkMatches.length === 3) {
+					company = {
+						name: linkMatches[1],
+						link: linkMatches[2]
+					};
+				}
+			}
 
-                    if (link) {
-                        link = link.replace(/<a [^>]*><img [^>]*>/g, '');
-                    }
+			// Check if values[2] exists before using includes
+			let location =
+				values[2] && values[2].includes('</br>')
+					? values[2].split('</br>').map((loc) => loc.trim())
+					: values[2]
+					? [values[2].trim()]
+					: [];
 
-                    let companyName = values[0].trim();
+			let applicationLink = values[3]
+				?.split('</a>')
+				.filter((element) => /alt="Apply"/.test(element))[0]
+				?.split('href=')[1]?.split('"')[1];
 
-                    companyName = companyName.replace(/\*\*/g, '');
+			// .map((link) => link.split('='))
+			// let links = values[3]?.replace('width="84"','')?.replaceAll('</a>','')?.replaceAll('<a ','')
+			data.push({
+				company: company,
+				role: values[1],
+				location: location,
+				applicationLink,
+				datePosted: values[4]
+			});
+		}
 
-                    const companyMatch = companyName.match(/\[(.*?)\]\(.*?\)/);
-                    if (companyMatch) {
-                        companyName = companyMatch[1];
-                    }
+		return data;
+	};
 
-                    const rowData = {
-                        Company: companyName,
-                        Role: values[1].trim(),
-                        Location: locations,
-                        ApplicationLink: link || '',
-                        DatePosted: values[4].trim(),
-                    };
-                    data.push(rowData);
-                }
-            }
-        }
+	try {
+		const res = await fetch(
+			'https://raw.githubusercontent.com/SimplifyJobs/Summer2024-Internships/dev/README.md'
+		);
 
-        return data;
-    };
+		if (!res.ok) {
+			throw new Error('Failed to fetch data.');
+		}
 
-    try {
-        const res = await fetch(
-            'https://raw.githubusercontent.com/SimplifyJobs/Summer2024-Internships/dev/README.md'
-        );
-
-        if (!res.ok) {
-            throw new Error('Failed to fetch data.');
-        }
-
-        const markdownText = await res.text();
-        const data = parseMarkdownTable(markdownText);
-
-        return new Response(JSON.stringify(data), { status: 200 });
-    } catch (error) {
-        return new Response(JSON.stringify({ error: error.message }), { status: 500 });
-    }
+		const markdownText = await res.text();
+		const data = await parseMarkdownTable(markdownText);
+		return new Response(JSON.stringify(data), { status: 200 });
+	} catch (error) {
+		return new Response(JSON.stringify({ error: error.message }), { status: 500 });
+	}
 };
